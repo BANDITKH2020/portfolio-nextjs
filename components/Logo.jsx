d0b0416601c37308bf07b1544b6182643222f904
Bandit Khawyen <banditkh2020@gmail.com>
2024-01-06 21:46:11 +0700

    no message

---
+++ components/Logo.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+Link
+
+const Logo = () => {
+    return <Link href='/'>
+        <Image src='/logo.svg' width={54} height={54} priority alt=""></Image>
+        </Link>
+}
+
+export default Logo;
