c534b46b8680d356b24daf43c00a6a1c57926832
Bandit Khawyen <banditkh2020@gmail.com>
2024-01-24 21:36:19 +0700

    no message

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import About from '@/components/About';
 import Hero from '@/components/Hero';
+import Services from '@/components/Service';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Services />
     </main>
   )
 }
