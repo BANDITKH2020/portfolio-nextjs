0492858f0928de52bc5beffefc1a7c679783ca4c
Bandit Khawyen <banditkh2020@gmail.com>
2024-01-25 21:59:14 +0700

    no message

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import About from '@/components/About';
 import Hero from '@/components/Hero';
+import Reviews from '@/components/Reviews';
 import Services from '@/components/Service';
+import Work from '@/components/Work';
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Work />
+      <Reviews />
     </main>
   )
 }
